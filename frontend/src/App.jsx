72f651560b529193fee01036672804ce6b530a75
Vedant <vedantmahajan2017@gmail.com>
2026-04-18 02:21:04 +0530

    vedant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import UserDashboard from './pages/UserDashboard';
+import SmartDevices from './pages/SmartDevices';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/smart-devices" element={<SmartDevices />} />
       </Routes>
     </Router>
   );
