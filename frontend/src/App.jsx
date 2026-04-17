a58e48a643a61db005cceb2edd364a6a76a02579
madhurasalvi <madhurasalvi51@gmail.com>
2026-04-18 02:07:46 +0530

    login and sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import UserDashboard from './pages/UserDashboard';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<UserDashboard />} />
       </Routes>
     </Router>
