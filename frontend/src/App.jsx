9df9aeb02229bae84351575955ef0af8d1cac231
Vedant <vedantmahajan2017@gmail.com>
2026-04-18 08:16:45 +0530

    backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,41 +3,23 @@ import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import UserDashboard from './pages/UserDashboard';
-import { Toaster } from 'sonner';
 import SmartDevices from './pages/SmartDevices';
+import { Toaster } from 'sonner';
 
 function App() {
   return (
     <>
-    <Toaster richColors position="top-right" />
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/smart-devices" element={<SmartDevices />} />
-      </Routes>
-    </Router>
+      <Toaster richColors position="top-right" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<UserDashboard />} />
+          <Route path="/smart-devices" element={<SmartDevices />} />
+        </Routes>
+      </Router>
     </>
-  );
-}
-
-export default App;
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import UserDashboard from './pages/UserDashboard';
-import SmartDevices from './pages/SmartDevices';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/smart-devices" element={<SmartDevices />} />
-      </Routes>
-    </Router>
   );
 }
 
